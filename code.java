7530bbfaa2f41f28d39159dfa7c6296f9ac003e0
Sunny Gouru <gourusannith@gmail.com>
2025-08-09 15:41:19 +0530

    commiting code and example

---
+++ code.java
@@ -0,0 +1,6 @@
+    public static void main(String[] args) {
+        // Example usage of the method
+        String input = "Hello, World!";
+        String output = reverseString(input);
+        System.out.println("Reversed String: " + output);
+    }
